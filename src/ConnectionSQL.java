293e51fb7f017d03b1cb97837d9e1526aca2ec34
letson <sonthaidev@gmail.com>
2016-03-02 20:49:58 -0800

    Update ConnectionSQL.java

--- src/ConnectionSQL.java
+++ src/ConnectionSQL.java
@@ -1,9 +1,12 @@
 package son.craig.chat.app;
 import java.sql.DriverManager;
 import java.sql.Connection;
+import java.sql.ResultSetMetaData;
 import java.sql.Statement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ConnectionSQL {
 
@@ -23,7 +26,7 @@ public class ConnectionSQL {
 	 * @throws java.sql.SQLException when failed to make a connection.
 	 */
 	public ConnectionSQL() throws SQLException {
-		
+
 		System.out.print("Connecting to database...");
 		try{
 			// constructs the connection URL
@@ -84,7 +87,42 @@ public class ConnectionSQL {
 		stmt.close ();
 		return rowCount;
 	}
+	/**
+	 * Method to execute an input query SQL instruction (i.e. SELECT).  This
+	 * method issues the query to the DBMS and returns the results as
+	 * a list of records. Each record in turn is a list of attribute values
+	 *
+	 * @param query the input query string
+	 * @return the query result as a list of records
+	 * @throws java.sql.SQLException when failed to execute the query
+	 */
+	public List<List<String>> executeQueryAndReturnResult (String query) throws SQLException { 
+		// creates a statement object 
+		Statement stmt = this._connection.createStatement (); 
 
+		// issues the query instruction 
+		ResultSet rs = stmt.executeQuery (query); 
+
+		/* 
+		 ** obtains the metadata object for the returned result set.  The metadata 
+		 ** contains row and column info. 
+		 */ 
+		ResultSetMetaData rsmd = rs.getMetaData (); 
+		int numCol = rsmd.getColumnCount (); 
+		int rowCount = 0; 
+
+		// iterates through the result set and saves the data returned by the query. 
+		boolean outputHeader = false;
+		List<List<String>> result  = new ArrayList<List<String>>(); 
+		while (rs.next()){
+			List<String> record = new ArrayList<String>(); 
+			for (int i=1; i<=numCol; ++i) 
+				record.add(rs.getString (i)); 
+			result.add(record); 
+		}//end while 
+		stmt.close (); 
+		return result; 
+	}//end executeQueryAndReturnResult
 	/**
 	 * Method to close the physical connection if it is open.
 	 */
@@ -99,10 +137,10 @@ public class ConnectionSQL {
 	}//end cleanup
 	public int getCurrSeqVal(String sequence) throws SQLException {
 		Statement stmt = this._connection.createStatement ();
-		
+
 		ResultSet rs = stmt.executeQuery (String.format("Select currval('%s')", sequence));
 		if (rs.next())
 			return rs.getInt(1);
 		return -1;
-	   }
+	}
 }
